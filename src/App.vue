e996971cb86a2d30299df9d72b671d4dfdffeefd
dianaveselkina <vess_di@mail.ru>
2024-05-14 12:27:59 +0300

    create project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <GameBoard />
+  </div>
 </template>
+<script>
+import GameBoard from './components/GameBoard.vue';
+export default {
+  components: {
+    GameBoard,
+  },
 
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.app {
+  max-width: 2600px;
+  margin: 0 auto;
+  background-color: #ffffff;
+  border-radius: 6px;
 }
 </style>
---
+++ src/components/GameBoard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="board"></div>
+</template>
+<script setup></script>
+<style>
+.board {
+  margin: 100px auto;
+  padding: 50px 0;
+  width: 2400px;
+  height: 2400px;
+  background-color: rgb(29, 56, 26);
+}
+</style>
