8c173f1aac2c74530d3165129e2675d14e6d1a99
dianaveselkina <vess_di@mail.ru>
2024-05-28 12:16:28 +0300

    add drag and drop

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,44 @@ export default {
   methods: {},
 };
 </script>
-<style>
+<style scoped>
+html {
+  line-height: 1.15;
+  -webkit-text-size-adjust: 100%;
+}
+body {
+  margin: 0;
+}
+main {
+  display: block;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+}
+ol,
+ul {
+  list-style: none;
+}
+img {
+  max-width: 100%;
+  width: 100%;
+  height: auto;
+}
+button {
+  cursor: pointer;
+  color: inherit;
+  font-family: inherit;
+  border: 0;
+}
 .app {
-  max-width: 2600px;
+  max-width: 1200px;
   margin: 0 auto;
   background-color: #ffffff;
-  border-radius: 6px;
 }
 </style>
--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,40 +1,121 @@
-<template>
-  <div class="board">
-    <div class="basket"></div>
-    <MushroomBlock />
-    <MushroomBlock class="mushroom1" />
-  </div>
-</template>
-<script>
-import MushroomBlock from './MushroomBlock.vue';
-export default {
-  components: {
-    MushroomBlock,
-  },
-  data() {
-    return {};
-  },
-  methods: {},
+<script setup>
+import { ref } from 'vue';
+
+const item1 = {
+  id: 1,
+};
+const item2 = {
+  id: 2,
+};
+const item3 = {
+  id: 3,
+};
+const item4 = {
+  id: 4,
+};
+const item5 = {
+  id: 5,
+};
+let containerOne = ref([item1, item2, item3, item4, item5]);
+let containerTwo = ref([]);
+
+const handleDragStart = (event, itemData) => {
+  event.dataTransfer.setData('application/json', JSON.stringify(itemData));
+};
+
+const handleDrop = (event, targetContainer) => {
+  const itemData = JSON.parse(event.dataTransfer.getData('application/json'));
+
+  if (targetContainer === containerOne.value) {
+    containerTwo.value = containerTwo.value.filter((i) => i.id !== itemData.id);
+  } else if (targetContainer === containerTwo.value) {
+    containerOne.value = containerOne.value.filter((i) => i.id !== itemData.id);
+  }
+  targetContainer.push(itemData);
 };
 </script>
-<style>
-.board {
-  margin: 100px auto;
-  padding: 50px 0;
-  width: 2400px;
-  height: 2400px;
-  background-color: rgb(29, 56, 26);
+
+<template>
+  <div class="drag-drop-container">
+    <div class="container-one">
+      <div
+        class="item"
+        v-for="item in containerOne"
+        :key="item.id"
+        draggable="true"
+        v-on:dragstart="handleDragStart($event, item)"
+      ></div>
+    </div>
+
+    <div
+      class="container-two"
+      v-on:dragover.prevent
+      v-on:drop="handleDrop($event, containerTwo)"
+    >
+      <div
+        class="item1"
+        v-for="item in containerTwo"
+        :key="item.id"
+        draggable="true"
+        v-on:dragstart="handleDragStart($event, item)"
+      ></div>
+    </div>
+    <div class="basket"></div>
+  </div>
+</template>
+
+<style scoped>
+.drag-drop-container {
+  position: relative;
+  width: 1100px;
 }
-.basket {
-  background-image: url(../img/basket.jpg);
-  z-index: 5;
+.container-one {
+  background-image: url(/public/img/forest.png);
   background-repeat: no-repeat;
   background-size: cover;
-  width: 250px;
-  height: 250px;
-  margin: 2000px auto 0;
+  position: relative;
+  width: 1100px;
+  height: 1200px;
+  padding: 10px;
 }
-.mushroom1 {
-  margin: -298px 0 500px 610px;
+.container-two {
+  z-index: 2;
+  position: absolute;
+  bottom: 545px;
+  left: 443px;
+  width: 230px;
+  height: 130px;
+}
+.basket {
+  background-image: url(/public/img/basket.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  z-index: 1;
+  position: absolute;
+  bottom: 400px;
+  left: 400px;
+  width: 314px;
+  height: 200px;
+}
+.item {
+  position: absolute;
+  top: 200px;
+  right: 200px;
+  display: inline-block;
+  background-image: url(/public/img/mushroom.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 63px;
+  height: 73px;
+
+  cursor: pointer;
+}
+.item1 {
+  display: inline-block;
+  background-image: url(/public/img/mushroom.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 63px;
+  height: 73px;
 }
 </style>
